c8c5b75a59835404de06efcc73350ea09195921c
unknown <sohelranagtc@gmail.com>
2023-10-22 17:28:55 +0600

    add get data by id

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.rbfkgiq.mongodb.net/?retryWrites=true&w=majority`;
@@ -49,6 +49,14 @@ async function run() {
       }
       const query = {email : email}
       const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+
+
+    app.delete('/carts/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
 
