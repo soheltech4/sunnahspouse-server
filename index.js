0c597da30ce99a38181edbabdcc481c0d8113d05
Sohel Rana <sohelranagtc@gmail.com>
2023-10-25 20:22:55 +0600

    deploy

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const allClass = client.db("languageDB").collection("allclass");
     const allInstructor = client.db("languageDB").collection("allinstructor");
