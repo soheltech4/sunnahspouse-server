ce48523b6b338e62458a62e3eece27274917ee6a
unknown <sohelranagtc@gmail.com>
2023-10-25 16:47:25 +0600

    add class

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ async function run() {
       res.send(result)
     })
 
+    app.post('/allclass', async(req, res)=>{
+      const Class = req.body
+      const result = await allClass.insertOne(Class)
+      res.send(result)
+    })
+
+
     app.get('/allclass/:id', async (req, res) => {
       const id = req.params.id
       const query = {_id : new ObjectId(id)};
