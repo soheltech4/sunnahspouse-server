24f91e5b1455ee4eec83c1a4585b7749d98406d5
unknown <sohelranagtc@gmail.com>
2023-10-26 00:43:43 +0600

    add

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const allClass = client.db("languageDB").collection("allclass");
     const allInstructor = client.db("languageDB").collection("allinstructor");
@@ -139,7 +139,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    await client.close();
   }
 }
 run().catch(console.dir);
